b2437013bb5bbcacedf9c545dc410de803ca4f0e
railg <roblox.master@mail.ru>
2025-07-03 15:01:20 +0300

    Add loading state and dynamic currency display to CurrenciesList component

---
+++ src/components/Main/Loader/index.tsx
@@ -0,0 +1,17 @@
+import '../../../style/Loader.css'
+export function Loader() {
+    return (
+        <div className="loader">
+            <div className='loader__text'>
+                <span className="loader__word">З</span>
+                <span className="loader__word">А</span>
+                <span className="loader__word">Г</span>
+                <span className="loader__word">Р</span>
+                <span className="loader__word">У</span>
+                <span className="loader__word">З</span>
+                <span className="loader__word">К</span>
+                <span className="loader__word">А</span>
+            </div>
+        </div>
+    )
+}
--- src/components/Page/CurrencysList.tsx
+++ src/components/Page/CurrencysList.tsx
@@ -2,17 +2,25 @@ import {Header} from '../Main/Header/Header'
 import '../../style/CurrenciesList.css'
 import { Table } from '../Main/CurrencysList/Table'
 import { NavLinkConverter } from '../Main/CurrencysList/NavLinkConverter'
+import { useAppSelector } from '../../hook'
+import { Loader } from '../Main/Loader'
 export function CurrenciesList() {
+    const { countrysCurrencies, loading } = useAppSelector(state => state.currencysList)
+    const { userCurrency } = useAppSelector(state => state.userCurrency)
+    const countrysCurrenciesLength = countrysCurrencies.length
     return (
-        <div className='cl'>
-            <Header children={<NavLinkConverter/>}/>
-            <div>
-                <h2 className='cl-body_text'>Ниже приведены обновление курсов валют: 1 RUB к валютам разных стран</h2>
-                <div className='cl-table_col'>
-                    <Table/>
-                    <Table/>
+        <>
+            <div className='cl'>
+                <Header children={<NavLinkConverter />} />
+                <div className='cl-table'>
+                    <h2 className='cl-body_text'>Ниже приведены обновление курсов валют: 1 {userCurrency} к валютам разных стран</h2>
+                    {!loading && <Loader />}
+                    {loading && <div className='cl-table_col'>
+                        <Table countrysCurrencies={countrysCurrencies.slice(0, countrysCurrenciesLength / 2)} />
+                        <Table countrysCurrencies={countrysCurrencies.slice(countrysCurrenciesLength / 2)} />
+                    </div>}
                 </div>
             </div>
-        </div>
+        </>
     )
 }
